39831be8383aacc320960932c7376ed59bcc465e
RusticoRoxas <113019323+RusticoRoxas@users.noreply.github.com>
2022-09-21 15:48:31 +0800

    Update GreetingProgram.cs

--- GreetingProgram.cs
+++ GreetingProgram.cs
@@ -4,25 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1{
-    class Program{
+namespace GreetingApp{
+    class GreetingProgram{
         static void Main(string[] args){
             string name, courses, bookPrice;
 
-            Console.Write(Enter your name );
+            Console.Write("Enter your name: ");
             name = Console.ReadLine();
 
-            Console.Write(Enter the total number of your enrolled courses );
+            Console.Write("Enter the total number of your enrolled courses: ");
             courses = Console.ReadLine();
 
-            Console.Write(Enter the price of your favorite book );
+            Console.Write("Enter the price of your favorite book: ");
             bookPrice = Console.ReadLine();
 
-            Console.WriteLine(nn--------nn);
+            Console.WriteLine("\n\n--------\n\n");
 
-            Console.WriteLine($Name {name});
-            Console.WriteLine($Total enrolled courses {courses});
-            Console.WriteLine($Price of {name}'s favorite book {bookPrice});
+            Console.WriteLine($"Name {name}");
+            Console.WriteLine($"Total enrolled courses {courses}");
+            Console.WriteLine($"Price of {name}'s favorite book {bookPrice}");
         }
     }
 }
